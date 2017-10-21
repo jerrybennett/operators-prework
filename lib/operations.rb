ceb2a7143e22cc3023332f4cba712571c25c8fb9
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-21 21:04:15 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,8 +11,5 @@ end
 
 
 def not_safe?(speed)
-  if speed > 60 ? true : false
-  elsif speed < 40 ? true : false
-  else speed < 60 && speed > 40 ? false : true
-  end
+  speed < 60 && speed > 40 ? false : true
 end
